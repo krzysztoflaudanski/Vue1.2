a06ca0329e88873970ee85b9fefc36f1eca9e631
Krzysztof <laudanski.krzysztof86@gmail.com>
2024-05-09 13:49:54 +0200

    init

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Counter</h1>
+    <button @click="decrement">-</button>
+    <span>{{ count }}</span>
+    <button @click="increment">+</button>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data: () => ({
+        count: 0
+    }),
+    methods: {
+        increment() {
+            this.count++
+        },
+
+        decrement() {
+            this.count--
+        }
+    }
+}
+</script>
